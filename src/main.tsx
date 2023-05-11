dc39ca90fbfe3f60ce3f22ca2e6ecefba52e1057
prabhu-raja <prabhuraja@gmail.com>
2023-05-11 15:42:18 -0400

    7 - Customize query settings

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,21 @@ import App from './App';
 import './index.css';
 
 const queryClient = new QueryClient();
+/*
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 3,
+      cacheTime: 300_000, // 5min
+      staleTime: 10 * 1000, // 10sec [How long the data considered fresh]
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+      refetchOnMount: false,
+    }
+  }
+});
+*/
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
