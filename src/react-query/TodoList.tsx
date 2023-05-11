2b8789d3fcfc82918b6d4471abcd2c57b9958d2c
prabhu-raja <prabhuraja@gmail.com>
2023-05-11 12:44:05 -0400

    5 - Created custom useQuery 🪝

--- src/react-query/TodoList.tsx
+++ src/react-query/TodoList.tsx
@@ -1,36 +1,8 @@
-import { useQuery } from '@tanstack/react-query';
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-
-interface Todo {
-  id: number;
-  title: string;
-  userId: number;
-  completed: boolean;
-}
+import useTodos from './hooks/useTodos';
 
 const TodoList = () => {
-  const fetchTodos = () =>
-    axios.get<Todo[]>('https://jsonplaceholder.typicode.com/todos').then((res) => res.data);
+  const { data: todos, error, isLoading } = useTodos();
 
-  const {
-    data: todos,
-    error,
-    isLoading,
-  } = useQuery<Todo[], Error>({
-    queryKey: ['todos'],
-    queryFn: fetchTodos,
-  });
-
-  // const [todos, setTodos] = useState<Todo[]>([]);
-  // const [error, setError] = useState('');
-
-  // useEffect(() => {
-  //   axios
-  //     .get('https://jsonplaceholder.typicode.com/todos')
-  //     .then((res) => setTodos(res.data))
-  //     .catch((error) => setError(error));
-  // }, []);
   if (isLoading) return <p>Loading..</p>;
 
   if (error) return <p>{error.message}</p>;
