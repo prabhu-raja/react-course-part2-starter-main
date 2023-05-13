50af819375119d4adc7227d5d63e380384069031
prabhu-raja <prabhuraja@gmail.com>
2023-05-13 07:57:16 -0400

    9 - minor cleanup

--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -8,14 +8,33 @@ const PostList = () => {
   if (error) return <p>{error.message}</p>;
 
   return (
-    <ul className="list-group">
-      {posts?.map((post) => (
-        <li key={post.id} className="list-group-item">
-          {post.title}
-        </li>
-      ))}
-    </ul>
+    <>
+      <select className="form-select mb-3">
+        <option value=""></option>
+        <option value="1">User 1</option>
+        <option value="2">User 2</option>
+        <option value="3">User 3</option>
+      </select>
+      <ul className="list-group">
+        {posts?.map((post) => (
+          <li key={post.id} className="list-group-item">
+            {post.title}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
 export default PostList;
+
+
+// const [posts, setPosts] = useState<Post[]>([]);
+// const [error, setError] = useState('');
+
+// useEffect(() => {
+//   axios
+//     .get('https://jsonplaceholder.typicode.com/posts')
+//     .then((res) => setPosts(res.data))
+//     .catch((error) => setError(error));
+// }, []);
