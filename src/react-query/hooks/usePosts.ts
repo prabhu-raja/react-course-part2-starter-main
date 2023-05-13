5879f4fede594e152a5253504a3b7b34d477909e
prabhu-raja <prabhuraja@gmail.com>
2023-05-13 16:42:28 -0400

    13- 🤯 useInfiniteQuery

--- src/react-query/hooks/usePosts.ts
+++ src/react-query/hooks/usePosts.ts
@@ -1,4 +1,4 @@
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 
 export interface Post {
@@ -9,26 +9,28 @@ export interface Post {
 }
 
 interface PostQuery {
-  page: number;
   pageSize: number;
 }
 
 const usePosts = (query: PostQuery) => {
-  const fetchPostList = () =>
+  const fetchPostList = (pageParam: number) =>
     axios
       .get<Post[]>('https://jsonplaceholder.typicode.com/posts', {
         params: {
-          _start: (query.page - 1) * query.pageSize,
+          _start: (pageParam - 1) * query.pageSize,
           _limit: query.pageSize,
         },
       })
       .then((res) => res.data);
 
-  return useQuery<Post[], Error>({
+  return useInfiniteQuery<Post[], Error>({
     queryKey: ['posts', query],
-    queryFn: fetchPostList,
+    queryFn: ({ pageParam = 1 }) => fetchPostList(pageParam),
     staleTime: 1 * 60 * 1000, // 1 min
     keepPreviousData: true,
+    getNextPageParam: (lastPage, allPages) => {
+      return lastPage.length > 0 ? allPages.length + 1 : undefined;
+    },
   });
 };
 
