2040944fbc7cbfec4b9b30efcee39c44a9c8fb24
prabhu-raja <prabhuraja@gmail.com>
2023-05-24 14:51:44 -0400

    23- state - exercise in implementing reducer in LoginStatus component

--- src/state-management/LoginStatus.tsx
+++ src/state-management/LoginStatus.tsx
@@ -1,24 +1,32 @@
-import { useState } from "react";
+import { useReducer, useState } from 'react';
+import authReducer from './reducers/authReducer';
 
 const LoginStatus = () => {
-  const [user, setUser] = useState('');
+  // const [user, setUser] = useState('');
+  const [user, dispatch] = useReducer(authReducer, '');
 
   if (user)
     return (
       <>
         <div>
           <span className="mx-2">{user}</span>
-          <a onClick={() => setUser('')} href="#">
+          <a onClick={() => dispatch({ type: 'LOGOUT' })} href="#">
             Logout
           </a>
+          {/* <a onClick={() => setUser('')} href="#">
+            Logout
+          </a> */}
         </div>
       </>
     );
   return (
     <div>
-      <a onClick={() => setUser('mosh.hamedani')} href="#">
+      <a onClick={() => dispatch({ type: 'LOGIN', userName: 'mosh.hamedani' })} href="#">
         Login
       </a>
+      {/* <a onClick={() => setUser('mosh.hamedani')} href="#">
+        Login
+      </a> */}
     </div>
   );
 };
---
+++ src/state-management/reducers/authReducer.ts
@@ -0,0 +1,23 @@
+interface LoginAction {
+  type: 'LOGIN';
+  userName: string;
+}
+
+interface LogoutAction {
+  type: 'LOGOUT';
+}
+
+type AuthAction = LoginAction | LogoutAction;
+
+const authReducer = (state: string, action: AuthAction): string => {
+  switch (action.type) {
+    case 'LOGIN':
+      return action.userName;
+    case 'LOGOUT':
+      return '';
+    default:
+      return state;
+  }
+};
+
+export default authReducer;
