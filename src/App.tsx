ff6c3642a74e85ac11acc7d4da61ed5bc1a865ac
prabhu-raja <prabhuraja@gmail.com>
2023-05-24 14:26:12 -0400

    22- state - actions challenge

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import PostList from './react-query/PostList';
 import TodoForm from './react-query/TodoForm';
 import TodoList from './react-query/TodoList';
 import Counter from './state-management/Counter';
+import TaskList from './state-management/TaskList';
 
 function App() {
   return (
     <>
-      <Counter />
+      <TaskList />
+      {/* <Counter /> */}
       {/* <TodoForm></TodoForm>
       <TodoList></TodoList> */}
       {/* <PostList /> */}
---
+++ src/state-management/reducers/tasksReducer.ts
@@ -0,0 +1,31 @@
+interface Task {
+  id: number;
+  title: string;
+}
+
+interface AddTask {
+  type: 'ADD';
+  task: Task;
+  // payload: Task;
+}
+
+interface DeleteTask {
+  type: 'DELETE';
+  taskId: number;
+  // payload: number;
+}
+
+type TaskAction = AddTask | DeleteTask;
+
+const tasksReducer = (state: Task[], action: TaskAction): Task[] => {
+  switch (action.type) {
+    case 'ADD':
+      return [action.task, ...state];
+    case 'DELETE':
+      return state.filter((s) => s.id !== action.taskId);
+    default:
+      return state;
+  }
+};
+
+export default tasksReducer;
