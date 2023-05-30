d2d43ddd9ac4ba895212effca0a1e283d542b261
prabhu-raja <prabhuraja@gmail.com>
2023-05-30 10:49:31 -0400

    30- state - Organizing tasks code for scalability & maintainability

---
+++ src/state-management/tasks/index.ts
@@ -0,0 +1,7 @@
+// import TasksProvider from './TasksProvider';
+// import TaskList from './TaskList';
+
+// export {TaskList, TasksProvider};
+
+export { default as TasksProvider } from './TasksProvider';
+export { default as TaskList } from './TaskList';
